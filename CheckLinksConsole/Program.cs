7153a6b69f2c96175ab70b976a85f95ba03828dd
Liya Ketema <liyaketema1@gmail.com>
2020-06-11 12:08:48 -0400

    added sln and unit test proj

--- CheckLinksConsole/Program.cs
+++ CheckLinksConsole/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-
+using System.Net.Http;
+using System.Linq;
 namespace CheckLinksConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var site = "https://lketema.github.io/";
+            var client = new HttpClient();
+            // the below dumps content of site
+            var bodyReturned = client.GetStringAsync(site);
+            
+
+            var links = LinkChecker.GetLinks(bodyReturned.Result);
+
+            links.ToList().ForEach(Console.WriteLine);
         }
     }
 }
